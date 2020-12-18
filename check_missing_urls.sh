c7cc447f4a294db09a8d810ac4a4fabd7a959c60
Alexandr Savca <drop@chinarulezzz.fun>
2020-12-18 11:46:20 +0200

    fix check_missing_urls.sh

--- check_missing_urls.sh
+++ check_missing_urls.sh
@@ -3,13 +3,13 @@
 # check vccrux
 for d in /usr/ports/vccrux/*; do
 	port=`basename $d`
-	egrep -q "^# $port" urls || echo $port
+	egrep -q "^# $port " urls || egrep -q "^# $port$" urls || echo $port
 done
 
 # check contrib
 grep 'Alexandr Savca' --include=Pkgfile -rl /usr/ports/contrib | while read d; do
 	port="$(basename $(dirname $d))"
-	egrep -q "^# $port" urls || echo $port
+	egrep -q "^# $port " urls || egrep -q "^# $port$" urls || echo $port
 done
 
 # check contrib volgk
